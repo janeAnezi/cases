03225c38d7d8aabf8e7f4718c20d8c776e015f29
Anezijane <ahnnieanezi@gmail.com>
2024-09-10 22:20:57 +0100

    add hide nav on scroll

---
+++ animation/navBarOnScroll/nav.js
@@ -0,0 +1,16 @@
+// script.js
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        navbar.style.top = '-60px'; // Adjust this value to the height of your navbar
+    } else {
+        // Scrolling up
+        navbar.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});

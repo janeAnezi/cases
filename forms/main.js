aa34efb04ce67256203e900f807ba4e87988da52
Anezijane <ahnnieanezi@gmail.com>
2024-09-22 21:07:00 +0100

    add email validation

--- forms/main.js
+++ forms/main.js
@@ -5,9 +5,18 @@ function validateForm() {
     const phone = document.getElementById("phone").value.trim();
     const password = document.getElementById("password").value.trim();
 
-       // Name validation
-       if (name === "") {
+    // Name validation
+    if (name === "") {
         alert("Please enter your name.");
+        return false;
+    }
+
+    // Email validation
+    if (email === "") {
+        alert("Please enter your email.");
+        return false;
+    } else if (!isValidEmail(email)) {
+        alert("Please enter a valid email address.");
         return false;
     }
 }

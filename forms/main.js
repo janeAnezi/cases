c9d3555acaabdee40fd8289c69d760998e382ad8
Anezijane <ahnnieanezi@gmail.com>
2024-09-22 21:05:13 +0100

    add javascript for forms

---
+++ forms/main.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    const name = document.getElementById("fullname").value.trim();
+    const userName = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    
+}

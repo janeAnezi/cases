d4a6d12c452eef0323fe16589045bb0a875c2d2a
Anezijane <ahnnieanezi@gmail.com>
2024-09-28 13:22:10 +0100

    add script

--- mealFinder/app.js
+++ mealFinder/app.js
@@ -1,3 +1,28 @@
+const searchMeal = async (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('.input')
+    const title = document.querySelector('.title')
+    const info = document.querySelector('.info')
+    const img = document.querySelector('.img')
+    const ingredientsOutput = document.querySelector('.ingredients')
+
+    const fetchMealData = async (val) => {
+     const res =   await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${val}`);
+
+
+     const {meals} = await res.json();
+     return meals;
+    };
+
+    const searchContent =  input.value.trim();
+
+    if(searchContent) {
+        const meals = await fetchMealData(searchContent)
+    }
+}
+
+
 const form = document.querySelector('form')
 form.addEventListener('submit', searchMeal)
 

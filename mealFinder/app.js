93a7bd1f2ea887e739b59f3b250a15a5ccfed574
Anezijane <ahnnieanezi@gmail.com>
2024-09-28 12:50:30 +0100

    add script

---
+++ mealFinder/app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', searchMeal)
+
+const magnifier = document.querySelector('.magnifier')
+magnifier.addEventListener('click', searchMeal)
